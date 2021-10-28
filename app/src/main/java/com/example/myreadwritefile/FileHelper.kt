087dbf8b115509a1f6263d37d715c4ef5f174865
YudhaKautsar <yudhakautsar20@gmail.com>
2021-10-28 12:25:54 +0700

    Latihan Menyimpan dan Membuka File

---
+++ app/src/main/java/com/example/myreadwritefile/FileHelper.kt
@@ -0,0 +1,23 @@
+package com.example.myreadwritefile
+
+import android.content.Context
+
+internal object FileHelper {
+
+    fun writeToFile(fileModel: FileModel, context: Context) {
+        context.openFileOutput(fileModel.filename, Context.MODE_PRIVATE).use {
+            it.write(fileModel.data?.toByteArray())
+        }
+    }
+
+    fun readFromFile(context: Context, fileName: String): FileModel {
+        val fileModel = FileModel()
+        fileModel.filename = fileName
+        fileModel.data = context.openFileInput(fileName).bufferedReader().useLines {
+            it.fold("") { some, text ->
+                "$some$text"
+            }
+        }
+        return fileModel
+    }
+}
